7fd24f6d86fa30b8cf3a042a28b89331d6f985e8
nhi.dang2 <dangtthuynhi@gmail.com>
2024-01-12 23:07:29 +0700

    init admin

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,23 +1,47 @@
 var express = require('express');
 var router = express.Router();
+const Product = require("../../models/product");
+const Category = require("../../models/category");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('admin/index', { title: 'Express' });
 });
 
-router.get('/products', function(req, res, next) {
-  res.render('admin/product', { title: 'Express' });
+router.get('/products', async function (req, res, next) {
+  try {
+    const products = await Product.find({})
+      .sort("createdAt")
+      .populate("category");
+
+    res.render('admin/product', {
+      title: 'Express',
+      products
+    });
+  } catch (error) {
+    console.log(error);
+    res.redirect("/");
+  }
 });
 
-router.get('/add-product', function(req, res, next) {
+router.get('/add-product', function (req, res, next) {
   res.render('admin/add-product', { title: 'Express' });
 });
 
-router.get('/category', function(req, res, next) {
+router.get('/category', function (req, res, next) {
   res.render('admin/category', { title: 'Express' });
 });
 
 
+router.get('/api/products', async (req, res) => {
+  try {
+    const products = await Product.find().populate('category');
+    // console.log(products);
+    res.json(products);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
 module.exports = router;
